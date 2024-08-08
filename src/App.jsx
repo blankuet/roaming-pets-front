c795f3cd3afbd8d9c463ca1c4ed169cc1e505460
Vituin <124085647+Vituin@users.noreply.github.com>
2024-08-08 20:08:34 +0200

    v1 frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Routes } from "react-router-dom";
 import Accommodations from "./components/Accommodations";
 import CreateBooking from "./components/CreateBooking";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Router>
-      <div>
-        <h1>Accommodation and Booking App</h1>
-        <Route exact path="/accommodations" component={Accommodations} />
-        <Route path="/create-booking" component={CreateBooking} />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <h1>Accommodation and Booking</h1>
+        <Routes path="/accommodations" component={Accommodations} />
+        <Routes path="/create-booking" component={CreateBooking} />
+      </Router>
+    </>
   );
 }
+
+export default App;
