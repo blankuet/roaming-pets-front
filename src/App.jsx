99f0934e6796875645b4c9958267614095ca1b76
Vituin <124085647+Vituin@users.noreply.github.com>
2024-08-10 12:38:40 +0200

    done update frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Login from './pages/Login.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Accommodation from "./pages/Accommodation.jsx";
+import Booking from "./pages/Booking.jsx";
 
 function App() {
-
   return (
     <>
       <AuthProvider>
-          <Routes>
-            <Route path="/" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
               <PrivateRoute>
                 <Dashboard />
               </PrivateRoute>
-            } />
-          </Routes>
+            }
+          />
+          <Route path="/accommodation" element={<Accommodation />} />
+          <Route path="/create-booking" element={<Booking />} />
+        </Routes>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Accommodation.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+export default function Accommodation() {
+  const [accommodation, setAccommodation] = useState([]);
+
+  useEffect(() => {
+    const fetchAccommodation = async () => {
+      try {
+        const response = await fetch("http://localhost:5005/api/accommodation");
+        const data = await response.json();
+        setAccommodation(data);
+      } catch (error) {
+        console.error("Error fetching accommodation:", error);
+      }
+    };
+
+    fetchAccommodation();
+  }, []);
+
+  return (
+    <div>
+      <h1>Accommodation</h1>
+      <ul>
+        {accommodation.map((accommodation) => (
+          <li key={accommodation._id}>{accommodation.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
