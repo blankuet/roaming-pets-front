697c86058a67a27c98472fef15e344b18b55da5e
blankuet <blancafeijoocruz@gmail.com>
2024-08-10 12:31:00 +0200

    now guest

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
-import SignUp from './pages/SignUp.jsx'
+import { Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/TravelerAuthContext.jsx'
 import SignUpT from './pages/traveler/SignUpT.jsx'
-import Login from './pages/Login.jsx'
 import LoginT from './pages/traveler/LoginT.jsx'
-import Dashboard from './pages/Dashboard.jsx'
 import DashboardT from './pages/traveler/DashboardT.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import TravelerPrivateRoute from './components/TravelerPrivateRoute.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         <AuthProvider>
-        <Router>
           <Routes>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
+            <Route path="/traveler/signup" element={<SignUpT />} />
+            <Route path="/traveler/login" element={<LoginT />} /> 
+            <Route path="/traveler/dashboard" element={
+              <TravelerPrivateRoute>
+                <DashboardT />
+              </TravelerPrivateRoute>
             } />
           </Routes>
-        </Router>
       </AuthProvider>
     </>
   )
