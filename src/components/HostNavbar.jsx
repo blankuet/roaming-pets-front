444b649c4d6db160695b28cd86e0d64f1ff8c97a
Javier Godoy <javiergodoymiguez@gmail.com>
2024-08-22 18:46:48 +0200

    javi-reviewnew

--- src/components/HostNavbar.jsx
+++ src/components/HostNavbar.jsx
@@ -1,37 +1,3 @@
-// import { NavLink } from "react-router-dom";
-// import HomeIcon from "/public/Home.jpg";
-
-// function HostNavbar() {
-//   return (
-//     <nav className="bg-black shadow-md py-4 px-6 flex items-center">
-//       <NavLink to="/host/" className="flex items-center">
-//         <img src={HomeIcon} alt="Home" className="h-10" />
-//       </NavLink>
-
-//       <div className="ml-auto flex space-x-4">
-//         <NavLink
-//           to="/host/signup"
-//           className={({ isActive }) =>
-//             `text-lg ${isActive ? "text-indigo-600" : "text-slate-200"}`
-//           }
-//         >
-//           Sign Up
-//         </NavLink>
-//         <NavLink
-//           to="/host/login"
-//           className={({ isActive }) =>
-//             `text-lg ${isActive ? "text-indigo-600" : "text-slate-200"}`
-//           }
-//         >
-//           Login
-//         </NavLink>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default HostNavbar
-
 import { NavLink } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext.jsx";
